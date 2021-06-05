8c61c031539fa1d8cda641a02afae3ca5febbe32
yushiro-div <61450307+yushiro-div@users.noreply.github.com>
2021-06-05 15:02:35 +0900

    ui first

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header: React.FC = () => {
+  return (
+    <div>
+      <div className="header-wrapper">
+        <div className="header-title">OUTPUT BLOG</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
